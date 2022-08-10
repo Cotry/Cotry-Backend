ddd007175f6ea76d8289ca5382fda361c4b180b2
le4kno3 <jlelo403@gmail.com>
2022-08-10 19:18:33 +0530

    update

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -7,22 +7,22 @@ module.exports = app => {
   router.post("/", users.create);
 
   // Retrieve all Tutorials
-  router.get("/", users.findAll);
+  router.get("/all", users.findAll);
 
   // Retrieve all published Tutorials
   router.get("/published", users.findAllPublished);
 
   // Retrieve a single Tutorial with id
-  router.get("/:id", users.findOne);
+  router.get("/user/:id", users.findOne);
 
   // Update a Tutorial with id
-  router.put("/:id", users.update);
+  router.put("/user/:id", users.update);
 
   // Delete a Tutorial with id
-  router.delete("/:id", users.delete);
+  router.delete("/user/:id", users.delete);
 
   // Delete all Tutorials
-  router.delete("/", users.deleteAll);
+  router.delete("/all", users.deleteAll);
 
   app.use("/api/users", router);
 };
