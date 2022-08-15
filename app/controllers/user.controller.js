9e6a7eca98aad6183d689a9d13b6fe6fa8ed5af5
le4kno3 <jlelo403@gmail.com>
2022-08-15 21:36:10 +0530

    update

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,3 +1,160 @@
+// const db = require("../models");
+// const User = db.users;
+// const Op = db.Sequelize.Op;
+
+// // Create and Save a new Tutorial
+// exports.create = (req, res) => {
+//   // Validate request
+//   if (!req.body.userName) {
+//     res.status(400).send({
+//       message: "Content can not be empty!"
+//     });
+//     return;
+//   }
+
+//   // Create a Tutorial
+//   const user = {
+//     firstName: req.body.firstName,
+//     lastName: req.body.lastName,
+//     userName: req.body.userName,
+//     walletAddress: req.body.walletAddress,
+//     walletType: req.body.walletType,
+//     walletBalance: req.body.walletBalance ? Number(req.body.walletBalance) : null,
+//   };
+
+//   // Save Tutorial in the database
+//   User.create(user)
+//     .then(data => {
+//       res.send(data);
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while creating the Tutorial."
+//       });
+//     });
+// };
+
+// // Retrieve all Tutorials from the database.
+// exports.findAll = (req, res) => {
+//   const userName = req.query.userName;
+//   var condition = userName ? { userName: { [Op.iLike]: `%${userName}%` } } : null;
+
+//   User.findAll({ where: condition })
+//     .then(data => {
+//       res.send(data);
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while retrieving tutorials."
+//       });
+//     });
+// };
+
+// // Find a single Tutorial with an id
+// exports.findOne = (req, res) => {
+//   const id = req.params.id;
+
+//   User.findByPk(id)
+//     .then(data => {
+//       if (data) {
+//         res.send(data);
+//       } else {
+//         res.status(404).send({
+//           message: `Cannot find Tutorial with id=${id}.`
+//         });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message: "Error retrieving Tutorial with id=" + id
+//       });
+//     });
+// };
+
+// // Update a Tutorial by the id in the request
+// exports.update = (req, res) => {
+//   const id = req.params.id;
+
+//   User.update(req.body, {
+//     where: { id: id }
+//   })
+//     .then(num => {
+//       if (num == 1) {
+//         res.send({
+//           message: "User was updated successfully."
+//         });
+//       } else {
+//         res.send({
+//           message: `Cannot update User with id=${id}. Maybe User was not found or req.body is empty!`
+//         });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message: "Error updating Tutorial with id=" + id
+//       });
+//     });
+// };
+
+// // Delete a Tutorial with the specified id in the request
+// exports.delete = (req, res) => {
+//   const id = req.params.id;
+
+//   User.destroy({
+//     where: { id: id }
+//   })
+//     .then(num => {
+//       if (num == 1) {
+//         res.send({
+//           message: "Tutorial was deleted successfully!"
+//         });
+//       } else {
+//         res.send({
+//           message: `Cannot delete User with id=${id}. Maybe User was not found!`
+//         });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message: "Could not delete Tutorial with id=" + id
+//       });
+//     });
+// };
+
+// // Delete all Tutorials from the database.
+// exports.deleteAll = (req, res) => {
+//   User.destroy({
+//     where: {},
+//     truncate: false
+//   })
+//     .then(nums => {
+//       res.send({ message: `${nums} Users were deleted successfully!` });
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while removing all tutorials."
+//       });
+//     });
+// };
+
+// // find all published Tutorial
+// exports.findAllPublished = (req, res) => {
+//   User.findAll({ where: { published: true } })
+//     .then(data => {
+//       res.send(data);
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while retrieving tutorials."
+//       });
+//     });
+// };
+
+
 const db = require("../models");
 const User = db.users;
 const Op = db.Sequelize.Op;
@@ -35,95 +192,110 @@ exports.create = (req, res) => {
     });
 };
 
-// Retrieve all Tutorials from the database.
-exports.findAll = (req, res) => {
-  const userName = req.query.userName;
-  var condition = userName ? { userName: { [Op.iLike]: `%${userName}%` } } : null;
+// Find a single Tutorial with its wallet address
+exports.findOne = (req, res) => {
+  const WALLETADDRESS = req.body.walletAddress;
 
-  User.findAll({ where: condition })
+  User.findAll({
+    limit: 1,
+    where: {
+      walletAddress: WALLETADDRESS
+    }
+  })
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find User with wallet address=${WALLETADDRESS}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving User with wallet address =" + WALLETADDRESS
+      });
+    });
+};
+
+// Update a User by the wallet address in the request
+// Update request can include any number of fields.
+// You cannot update unique values like id, wallet address, note: username can be updated
+exports.update = (req, res) => {
+  const WALLETADDRESS = req.body.walletAddress;
+
+  User.update(req.body, {
+    limit: 1,
+    where: {
+      walletAddress: WALLETADDRESS
+    }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: `User with wallet address = ${WALLETADDRESS} was updated successfully.`
+        });
+      } else {
+        res.send({
+          message: `Cannot update User with wallet address =${WALLETADDRESS}. Maybe User was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating User with wallet address =" + WALLETADDRESS
+      });
+    });
+};
+
+// Delete a User with the specified wallet address.
+exports.delete = (req, res) => {
+  const WALLETADDRESS = req.body.walletAddress;
+
+  User.destroy({
+    limit: 1,
+    where: {
+      walletAddress: WALLETADDRESS
+    }
+  }).then(num => {
+    if (num == 1) {
+      res.send({
+        message: `User with wallet address = ${WALLETADDRESS} was deleted successfully!`
+      });
+    } else {
+      res.send({
+        message: `Cannot delete User with wallet address = ${WALLETADDRESS}. Maybe User was not found!`
+      });
+    }
+  })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Tutorial with wallet address = " + WALLETADDRESS
+      });
+    });
+};
+
+// Retrieve all Users from the database.
+exports.findAll = (req, res) => {
+  // const userName = req.query.userName;
+  // var condition = userName ? { userName: { [Op.iLike]: `%${userName}%` } } : null;
+
+  User.findAll({
+    where: {},
+    truncate: false
+  })
     .then(data => {
       res.send(data);
     })
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving tutorials."
+          err.message || "Some error occurred while retrieving users."
       });
     });
 };
 
-// Find a single Tutorial with an id
-exports.findOne = (req, res) => {
-  const id = req.params.id;
-
-  User.findByPk(id)
-    .then(data => {
-      if (data) {
-        res.send(data);
-      } else {
-        res.status(404).send({
-          message: `Cannot find Tutorial with id=${id}.`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error retrieving Tutorial with id=" + id
-      });
-    });
-};
-
-// Update a Tutorial by the id in the request
-exports.update = (req, res) => {
-  const id = req.params.id;
-
-  User.update(req.body, {
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: "User was updated successfully."
-        });
-      } else {
-        res.send({
-          message: `Cannot update User with id=${id}. Maybe User was not found or req.body is empty!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error updating Tutorial with id=" + id
-      });
-    });
-};
-
-// Delete a Tutorial with the specified id in the request
-exports.delete = (req, res) => {
-  const id = req.params.id;
-
-  User.destroy({
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: "Tutorial was deleted successfully!"
-        });
-      } else {
-        res.send({
-          message: `Cannot delete User with id=${id}. Maybe User was not found!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Could not delete Tutorial with id=" + id
-      });
-    });
-};
-
-// Delete all Tutorials from the database.
+// Delete all Users from the database.
 exports.deleteAll = (req, res) => {
   User.destroy({
     where: {},
@@ -136,20 +308,6 @@ exports.deleteAll = (req, res) => {
       res.status(500).send({
         message:
           err.message || "Some error occurred while removing all tutorials."
-      });
-    });
-};
-
-// find all published Tutorial
-exports.findAllPublished = (req, res) => {
-  User.findAll({ where: { published: true } })
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving tutorials."
       });
     });
 };
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,26 +3,53 @@ module.exports = app => {
 
   var router = require("express").Router();
 
-  // Create a new Tutorial
-  router.post("/", users.create);
+  // Create a new User
+  router.post("/user/create", users.create);
 
-  // Retrieve all Tutorials
-  router.get("/getall", users.findAll);
+  // Retrieve a single User with its wallet address
+  router.post("/user/find", users.findOne);
 
-  // Retrieve all published Tutorials
-  router.get("/published", users.findAllPublished);
+  // Update a Tutorial with its wallet address (wallet address cant be updated)
+  router.post("/user/update", users.update);
 
-  // Retrieve a single Tutorial with id
-  router.get("/user/:id", users.findOne);
+  // Delete a Tutorial with its wallet address
+  router.post("/user/delete", users.delete);
 
-  // Update a Tutorial with id
-  router.put("/user/:id", users.update);
-
-  // Delete a Tutorial with id
-  router.delete("/user/:id", users.delete);
+  // Retrieve all Users
+  router.post("/getall", users.findAll); //Disable this in production or ensure proper security.
 
   // Delete all Tutorials
-  router.delete("/all", users.deleteAll);
+  router.post("/deleteall", users.deleteAll); //Disable this in production or ensure proper security.
+
+  // router.post("/login")
 
   app.use("/api/users", router);
 };
+
+
+
+// module.exports = app => {
+//   const users = require("../controllers/user.controller.js");
+
+//   var router = require("express").Router();
+
+//   // Create a new User
+//   router.post("/user/create", users.create);
+
+//   // Retrieve all Users
+//   router.get("/getall", users.findAll);
+
+//   // Retrieve a single user with id
+//   router.get("/user/:id", users.findOne);
+
+//   // Update a Tutorial with id
+//   router.put("/user/:id", users.update);
+
+//   // Delete a Tutorial with id
+//   router.delete("/user/:id", users.delete);
+
+//   // Delete all Tutorials
+//   router.delete("/deleteall", users.deleteAll);
+
+//   app.use("/api/users", router);
+// };
