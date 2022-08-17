d495bbaa011cf03dc3a244dd6c778294ed2b148a
le4kno3 <jlelo403@gmail.com>
2022-08-17 19:53:14 +0530

    update

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+//libraries import
 const express = require("express");
 // const bodyParser = require("body-parser"); /* deprecated */
-const session = require('express-session');
 const fs = require('fs');
 const cors = require("cors");
-const db = require("./app/models");
+const pg = require('pg');
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
 
+// custom modules import
+const db = require("./app/models");
+const sessions_dbConfig = require("./app/config/sessions_db.config.js");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // // using Custom CA cert
 // var key = fs.readFileSync('./certs/selfsigned.key');
@@ -44,7 +53,7 @@ app.use(express.json());  /* bodyParser.json() is deprecated */
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
 
-
+// db = object with configured db details.
 db.sequelize.sync();
 // drop the table if it already exists - for development only
 
@@ -52,18 +61,49 @@ db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
 });
 
+/**
+ * -------------- SESSION SETUP ----------------
+ */
+
+const pgPool = new pg.Pool(sessions_dbConfig);
+
+// Best Practices: https://blog.jscrambler.com/best-practices-for-secure-session-management-in-node
+app.use(session({
+  //We can have multiple secrets as an array.
+  //The first secret will be used to sign the cookie.
+  //The rest will be used in verification.
+  secret: process.env.SESSIONSSECRET,
+  name: 'vericheck',
+  // store: new pgSession({
+  //   pool: pgPool                // Connection pool
+  // }),
+  // Ref: https://stackoverflow.com/questions/40381401/when-to-use-saveuninitialized-and-resave-in-express-session
+  // It basically means that for every request to the server, it reset the session cookie
+  resave: false,
+  // True, means that Your session is only Stored into your storage, when any of the Property is modified in req.session
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    secure: true,
+    sameSite: true,
+    maxAge: 1000 * 60 * 10 * 1 // = Time is in milliseconds for 10min
+    // maxAge: 1000 * 60 * 60 * 24 // Time is in milliseconds for 1 day
+  }
+}));
+
+// add routes
 require("./app/routes/user.routes")(app);
 require("./app/routes/main.routes")(app);
+
+// simple route for load balancer health
+app.get('/', (req, res, next) => {
+  res.send('Health is OK.');
+});
 
 // set port, listen for requests
 const PORT = process.env.APPPORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
-});
-
-// simple route for load balancer health
-app.get('/', function (req, res) {
-  res.send('Health is OK.');
 });
 
 // // custom CA cert https server
